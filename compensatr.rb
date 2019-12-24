a88a1fab15ebecdb06fb68cea3bd5547b340dce8
Manuel González <magandrez@gmail.com>
2019-12-24 13:18:59 +0200

    Refactors code for parsing to dedicated library

--- compensatr.rb
+++ compensatr.rb
@@ -2,31 +2,8 @@
 
 require 'optparse'
 require 'json'
+require_relative 'lib/parsing'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = 'Usage: compensatr.rb -f <path> [options]'
-  opts.on('-f', '--file <path>', String, 'Source file of projects')
-  opts.on('-h', '--help', 'Prints this help') do
-    puts opts
-    exit
-  end
-end.parse!(into: options)
-
-raise OptionParser::MissingArgument if options[:file].nil?
-
-begin
-  source_file = File.read(options[:file])
-rescue StandardError => e
-  puts e.inspect
-  exit 1
-end
-
-begin
-  hash = JSON.parse(source_file, symbolize_names: true)
-rescue JSON::ParserError => e
-  puts e
-  exit 1
-end
-
+parse_cmd!
+hash = parse_input(read_input)
 puts hash
