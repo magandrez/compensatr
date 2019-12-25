14ffd24558d1e82cfe162115df5f9b003a169135
Manuel González <magandrez@gmail.com>
2019-12-25 12:47:04 +0200

    Renames wrongly named variable

--- compensatr.rb
+++ compensatr.rb
@@ -5,5 +5,5 @@ require_relative 'lib/parser'
 Parser.read_args
 file = Parser.read_input_file
 exit 1 unless file
-hash = Parser.parse_input(file)
-puts hash
+arr = Parser.parse_input(file)
+puts arr
