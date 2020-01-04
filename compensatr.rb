8180d48807d8497485a7fd5c8b999f282ed601ef
Manuel González <magandrez@gmail.com>
2020-01-04 15:45:32 +0200

    Adds comments and fixes style

--- compensatr.rb
+++ compensatr.rb
@@ -18,7 +18,7 @@ MAX_ITERATIONS = 100_000
 # standardised time units, matches it against
 # each of the projects' time_unit returning
 # deviations.
-# It is not the main task of this proof-of-concep
+# It is not the main task of this proof-of-concept
 # to provide a robust validation. For that, look
 # into implementing something like dry-validation
 # https://dry-rb.org/gems/dry-validation/1.4/
@@ -31,7 +31,7 @@ def validate_time_units(projects)
   end
   unless res.empty?
     puts "ERROR: Time units are not recognised \
-for projects with id: #{res.map { |r| r[:id] }}"
+      for projects with id: #{res.map { |r| r[:id] }}"
   end
   res
 end
@@ -66,96 +66,172 @@ def calculate_efficiency(arr)
   arr
 end
 
-# Enriches array with price per unit of CO2
-# captured per year
-# @param [Array] array
-# @return [Array] array with price per unit per project
-def calculate_price_per_unit(array)
-  array.map do |proj|
-    proj[:yearly_price] = (proj[:yearly_co2_vol] / proj[:price]).round(2)
-  end
-
-  array
+# Validates minimum units for selected project
+# @param [Integer] count of project in selection
+# @param [Hash] proj
+# @return [TrueClass, FalseClass] true if project is within min boundaries
+def meets_min_units(count, proj)
+  (count >= proj[:min_units])
 end
 
+# Validates maximum units for selected project
+# @param [Integer] count of project in selection
+# @param [Hash] proj
+# @return [TrueClass, FalseClass] true if project is within max boundaries
+def meets_max_units(count, proj)
+  (count <= proj[:max_units])
+end
+
+# Each project repetition in the selection array means 1 credit
+# valid_project_constraints validates that the minimum and maximum
+# units per project are met
+# @param [Array] selection of projects
+# @return [TrueClass, FalseClass] true if valid, false otherwise
 def valid_project_constraints(selection)
   reps = selection.each_with_object(Hash.new(0)) { |h1, h2| h2[h1[:id]] += 1 }
   req_reps = selection.all? do |proj|
-    reps_in_proj = reps[proj[:id]]
-    return false unless(reps_in_proj >= proj[:min_units]) &&  (reps_in_proj <= proj[:max_units])
+    count = reps[proj[:id]]
+    unless meets_min_units(count, proj) && meets_max_units(count, proj)
+      return false
+    end
+
     true
   end
 
   req_reps
 end
 
+# Counts the distribution of selected projects per continent
+# @param [Array] projects
+# @param [Integer]
 def count_continents(projects)
   projects.map { |h| h[:continent] }.uniq.size
 end
 
+# Verifies that the distribution of projects per continent
+# falls within the minimum number of continents required by input
+# @param [Array] selection of projects
+# @param [Integer] minimum continents
+# @return [TrueClass, FalseClass] true if it meets the minimum, false otherwise
 def valid_min_continents(selection, min_continents)
   count_continents(selection) >= min_continents
 end
 
-def summarise_expenditure(projects)
-  sum_short = projects.map {|proj| proj[:price] if proj[:group] == "short_term" }.compact.reduce(:+)
-  sum_medium = projects.map {|proj| proj[:price] if proj[:group] == "medium_term" }.compact.reduce(:+)
-  sum_long = projects.map {|proj| proj[:price] if proj[:group] == "long_term" }.compact.reduce(:+)
-
-  return { short: sum_short.to_f,
-    medium: sum_medium.to_f,
-    long: sum_long.to_f }
+# Adds cost of short term projects in selection
+# @param [Array] projects
+# @return [Float]
+def sum_short_term(projects)
+  projects.map do |proj|
+    proj[:price] if proj[:group] == 'short_term'
+  end.compact.reduce(:+)
 end
 
-def validate_min_expenditures(sums, expenditure, groups)
-  # sum might be nil (lack of projects in said group)
-  # !! converts nil into a boolean
-  short_term_ok = !!sums[:short] && (sums[:short] >= expenditure * (groups[:min_short_term_percent]/100))
-  medium_term_ok = !!sums[:medium] && (sums[:medium] >= expenditure * (groups[:min_medium_term_percent]/100))
-  long_term_ok = !!sums[:long] && (sums[:long] >= expenditure * (groups[:min_long_term_percent]/100))
-
-  return { short: short_term_ok,
-    medium: medium_term_ok,
-    long: long_term_ok}
+# Adds cost of medium term projects in selection
+# @param [Array] projects
+# @return [Float]
+def sum_medium_term(projects)
+  projects.map do |proj|
+    proj[:price] if proj[:group] == 'medium_term'
+  end.compact.reduce(:+)
 end
 
+# Adds cost of long term projects in selection
+# @param [Array] projects
+# @return [Float]
+def sum_long_term(projects)
+  projects.map do |proj|
+    proj[:price] if proj[:group] == 'long_term'
+  end.compact.reduce(:+)
+end
+
+# Summarises the costs of purchasing credits per
+# type of project returning a hash with prices
+# @param [Array] projects
+# @return [Hash] containing added costs per project group
+def add_costs(projects)
+  { short: sum_short_term(projects),
+    medium: sum_medium_term(projects),
+    long: sum_long_term(projects) }
+end
+
+# Verifies the distribution of money per project group is
+# within the minimum required per group
+# @param [Float, nilClass] value spent in specific project groups
+# value might be nil (lack of projects in said group).
+# @param [Float] expenditure total money spent
+# @param [Float] min money required to be spent per group
+# @return [TrueClass, FalseClass] true if the value spent is above the minimum
+def money_distribution(value, expenditure, min)
+  return false unless value
+
+  (value >= expenditure * (min / 100))
+end
+
+# Aggregates the validations of money distribution per group
+# @param [Hash] sums money per project group
+# @param [Float] total_money spent
+# @param [Hash] groups containing min percentage of money to be spent in group
+# @return [Hash] containing summary of validations per project group
+def validate_min_expenditures(sums, total_money, groups)
+  { short: money_distribution(sums[:short],
+                              total_money,
+                              groups[:min_short_term_percent]),
+    medium: money_distribution(sums[:medium],
+                               total_money,
+                               groups[:min_medium_term_percent]),
+    long: money_distribution(sums[:long],
+                             total_money,
+                             groups[:min_long_term_percent]) }
+end
+
+# Regenerates groups mapping with integers
+# for comparing sets of project groups
+# @param [Hash] groups hash stating minimum percentages of expenditure
+# @return [Hash] groups of expenditures by integer
 def remap_groups(groups)
-  groups.reject { |k, v| v == 0 }
+  groups.reject { |_k, v| v.zero? }
   key_map = { min_short_term_percent: 0,
-    min_medium_term_percent: 1,
-    min_long_term_percent: 2}
-  groups.transform_keys {|k| key_map[k]}
+              min_medium_term_percent: 1,
+              min_long_term_percent: 2 }
+  groups.transform_keys { |k| key_map[k] }
 end
 
+# Applies all combinations of possible minimum percentages registered
+# at input time. Comparing arrays in Ruby is not possible with '=='
+# but Set class provides such functionality, hence the casts <Array>.to_set
+# @param [Array] selection of projects
+# @param [Hash] groups specifying minimal expenditure per group
+# @param [Float] money_spent on selection
 def valid_min_groups(selection, groups, money_spent)
-  return true if groups.values.all? {|g| g == 0 } # If no minimums apply, selection is valid
-  summary = summarise_expenditure(selection)
+  # If no minimums apply, selection is valid
+  return true if groups.values.all?(&:zero?)
+
+  summary = add_costs(selection)
   valid = validate_min_expenditures(summary, money_spent, groups)
-  groups = remap_groups(groups)
-  # Comparing arrays in Ruby is not possible with ==
-  # but Set class provides such functionality
-  # hence the casts <Array>.to_set
-  case
-  when groups.keys.to_set == [0].to_set
+  groups = remap_groups(groups) # In order to compare sets of integers
+  if groups.keys.to_set == [0].to_set
     valid[:short]
-  when groups.keys.to_set == [1].to_set
+  elsif groups.keys.to_set == [1].to_set
     valid[:medium]
-  when groups.keys.to_set == [2].to_set
+  elsif groups.keys.to_set == [2].to_set
     valid[:long]
-  when groups.keys.to_set == [0,1].to_set
+  elsif groups.keys.to_set == [0, 1].to_set
     valid[:short] && valid[:medium]
-  when groups.keys.to_set == [0,2].to_set
+  elsif groups.keys.to_set == [0, 2].to_set
     valid[:short] && valid[:long]
-  when groups.keys.to_set == [1,2].to_set
+  elsif groups.keys.to_set == [1, 2].to_set
     valid[:medium] && valid[:long]
-  when groups.keys.to_set == [0,1,2].to_set
+  elsif groups.keys.to_set == [0, 1, 2].to_set
     valid[:short] && valid[:medium] && valid[:long]
   end
 end
 
+# Agregates projects per project id and adds totals
+# @param [Array] selection of projects
+# @return [Array] aggregated selection of projects per project id
 def aggregate_projects(selection)
-  groups = selection.group_by{|h| h[:id]}
-  groups.map do |k,v|
+  groups = selection.group_by { |h| h[:id] }
+  groups.map do |_k, v|
     agg_hash = v.first
     agg_hash[:count] = v.count
     agg_hash[:total_co2_captured] = v.first[:yearly_co2_vol] * v.count
@@ -164,41 +240,47 @@ def aggregate_projects(selection)
   end
 end
 
+# Based on the best selection, creates a hash
+# containing the units purchased, the project id and
+# the total price for the operation
+# @param [Array] selection
+# @return [Array] array of hashes containing purchase plan
 def generate_purchase_plan(selection)
   aggregated_projects = aggregate_projects(selection)
   aggregated_projects.map do |proj|
-    {
-      "project_id" => proj[:id],
-      "num_units" => proj[:count],
-      "price" => (proj[:price] * proj[:count]).round(1)
-    }
+    { 'project_id' => proj[:id],
+      'num_units' => proj[:count],
+      'price' => (proj[:price] * proj[:count]).round(1) }
   end
 end
 
+# Based on the best selection, creates a CO2 report of
+# for each of the years requested at input, calculating CO2 captured.
+# Each year might be projects that are not active anymore
+# thus, we don't count them for said year onwards.
+# std_time per project is minused A negative std_time means
+# the project "has finished" (we don't count the potential CO2 captured)
+# @param [Array] selection of most suitable projects
+# @param [Integer] years for which to calculate the CO2 captured
 def generate_co2_report(selection, years)
   aggregated_projects = aggregate_projects(selection)
-  this_year = Date.today.year
-  timeline = (this_year..this_year + years).to_a
-  co2_report = timeline.map do |year|
-    active_projects = aggregated_projects.reject{|proj| proj[:finished]}
+  this_yr = Date.today.year
+  co2_report = (this_yr..this_yr + years).map do |year|
+    aggregated_projects.reject! { |proj| proj[:finished] }
     co2_counter = 0
-    active_projects.each do |proj|
+    aggregated_projects.each do |proj|
       co2_counter += proj[:total_co2_captured]
-      proj[:std_time] -= 1 # Counted the co2 captured this year
-      # A negative std_time means the project has finished already,
-      # since std_time is time in years
-      proj[:finished] = true if proj[:std_time] < 0
+      proj[:std_time] -= 1
+      proj[:finished] = true if proj[:std_time].negative?
     end
-    {
-      "year" => year,
-      "co2_captured" => co2_counter.round(1)
-    }
+    { 'year' => year,
+      'co2_captured' => co2_counter.round(1) }
   end
 
   co2_report
 end
 
-
+# MAIN
 if $PROGRAM_NAME == __FILE__ # Let the script run unless Rspec is the caller
   cmd_input = InputParser.new
   params = cmd_input.params
@@ -207,50 +289,59 @@ if $PROGRAM_NAME == __FILE__ # Let the script run unless Rspec is the caller
   projects = normalise_time(arr)
   exit 1 if projects.empty?
   enriched_projects = calculate_efficiency(projects)
-  full_data = calculate_price_per_unit(enriched_projects)
 
   # Use brute force to create an optimal selection
   best_selection = []
   best_value = 0
   money_spent = 0
-  1.upto(MAX_ITERATIONS) do |i|
+  1.upto(MAX_ITERATIONS) do |_i|
     money = params[:money]
     total_value = 0
     selection = []
-    while true
-      pick = full_data.shuffle.sample
-      break if (money - pick[:price]) < 0
+    loop do
+      pick = enriched_projects.shuffle.sample
+      break if (money - pick[:price]).negative?
+
       money -= pick[:price]
       selection.append(pick)
       total_value += pick[:yearly_co2_vol]
     end
     next unless valid_project_constraints(selection)
     next unless valid_min_continents(selection, params[:min_continents])
-    next unless valid_min_groups(selection, params[:groups], params[:money] - money)
-    if total_value > best_value
-      best_selection = selection.dup
-      best_value = total_value
-      money_spent = params[:money] - money
-      puts "Efficiency of new best selection of projects (in units of CO2): #{best_value}"
-      puts "Money spent in new best selection of projects (in money units): #{money_spent}"
-      puts "Count of different project locations by continent: #{count_continents(best_selection)}"
-      expenditures = summarise_expenditure(best_selection)
-      puts "Representation per group: "
-      puts "  Short term value (in money units): #{expenditures[:short].to_f}. In percentage #{((expenditures[:short].to_f/money_spent)*100).round(2)}%."
-      puts "  Medium term value (in money units): #{expenditures[:medium].to_f}. In percentage #{((expenditures[:medium].to_f/money_spent)*100).round(2)}%"
-      puts "  Long term value: (in money units) #{expenditures[:long].to_f}. In percentage #{((expenditures[:long].to_f/money_spent)*100).round(2)}%"
-      puts "++++++++++++"
+    unless valid_min_groups(selection, params[:groups], params[:money] - money)
+      next
     end
+
+    next unless total_value > best_value
+
+    best_selection = selection.dup
+    best_value = total_value
+    money_spent = params[:money] - money
+    puts "Efficiency of new best selection of projects (in units of CO2):\
+          #{best_value}"
+    puts "Money spent in new best selection of projects (in money units):\
+           #{money_spent}"
+    puts "Count of different project locations by continent:\
+         #{count_continents(best_selection)}"
+    expenditures = add_costs(best_selection)
+    puts 'Representation per group: '
+    puts "  Short term value (in money units): #{expenditures[:short]}.\
+         In percentage #{((expenditures[:short].to_f /
+money_spent) * 100).round(2)}%."
+    puts "  Medium term value (in money units): #{expenditures[:medium]}.\
+ In percentage #{((expenditures[:medium].to_f / money_spent) * 100).round(2)}%"
+    puts "  Long term value: (in money units) #{expenditures[:long]}.\
+ In percentage #{((expenditures[:long].to_f / money_spent) * 100).round(2)}%"
+    puts '++++++++++++'
   end
-  puts "------------"
-  puts "Best selection: #{best_selection}"
+  puts " ------------ \n Best selection: \n #{best_selection}"
   puts "Best efficiency achieved (in total units of CO2): #{best_value}"
   puts "Money spent: #{money_spent} / #{params[:money]}"
   purchase_plan = generate_purchase_plan(best_selection)
   co2_report = generate_co2_report(best_selection, params[:target_years])
   results = {
-    "purchase_plan" => purchase_plan,
-    "co2_report" => co2_report
+    'purchase_plan' => purchase_plan,
+    'co2_report' => co2_report
   }
   cmd_input.write_output_file(results)
 end
--- lib/input_parser.rb
+++ lib/input_parser.rb
@@ -3,66 +3,103 @@
 require 'json'
 require 'optparse'
 
+# Class in charge of reading and parsing input
+# and writing output.
 class InputParser
+  # Could be extracted to a file and read from there
+  # using, e.g. https://github.com/bkeepers/dotenv
+  DEFAULTS = {
+    target_years: 1,
+    min_continents: 1,
+    min_short_term_percent: 0.0,
+    min_medium_term_percent: 0.0,
+    min_long_term_percent: 0.0,
+    target: './output.json'
+  }.freeze
 
   attr_reader :params
 
+  # Initialisez @params instance variable
+  # with the user input
   def initialize
     @params = read_args
   end
 
+  # Creates the interface with the user.
+  # Reads command line arguments and parses them
+  # as parameters to be aggregated with some defaults
+  # @return [Hash] params with parsed input from $stdin
   def read_args
-    # Default parameters
-    # Could be extracted to a file and read from there
-    # using, e.g. https://github.com/bkeepers/dotenv
-    params = {
-      target_years: 1,
-      min_continents: 1,
-      min_short_term_percent: 0.0,
-      min_medium_term_percent: 0.0,
-      min_long_term_percent: 0.0,
-      target: './output.json'
-    }
+    options = {}
     OptionParser.new do |opts|
       opts.banner = 'Usage: compensatr.rb -f <path> [options]'
       opts.on('-f <path>', '--file <path>', String, 'Source file of projects')
-      opts.on('-t <path>', '--target <path>', String, 'Target file where result is stored. Defaults to ./output.json')
-      opts.on('-m <value>', '--money <value>', Float, 'Amount of money to be used')
-      opts.on('--target_years <value>', Integer, 'Target years for C02 optimization, defaults to 1')
-      opts.on('-c <value>', '--min_continents <value>', Integer, 'Minimum number of continents where projects should be distributed, defaults to 1')
-      opts.on('--min_short_term_percent <value>', Float, 'Minimum percentage of short term projects to be bought, defaults to 0')
-      opts.on('--min_medium_term_percent <value>', Float, 'Minimum percentage of medium term projects to be bought, defaults to 0')
-      opts.on('--min_long_term_percent <value>', Float, 'Minimum percentage of long term projects to be bought, defaults to 0')
+      opts.on('-t <path>',
+              '--target <path>',
+              String,
+              'Target file where result is stored. Defaults to ./output.json')
+      opts.on('-m <value>', '--money <value>',
+              Float,
+              'Amount of money to be used')
+      opts.on('--target_years <value>',
+              Integer,
+              'Target years for C02 optimization, defaults to 1')
+      opts.on('-c <value>',
+              '--min_continents <value>',
+              Integer,
+              "Minimum number of continents where projects should be \
+distributed, defaults to 1")
+      opts.on('--min_short_term_percent <value>',
+              Float,
+              "Minimum percentage of short term projects to be bought\
+, defaults to 0")
+      opts.on('--min_medium_term_percent <value>',
+              Float,
+              "Minimum percentage of medium term projects to be bought\
+, defaults to 0")
+      opts.on('--min_long_term_percent <value>',
+              Float,
+              "Minimum percentage of long term projects to be bought\
+, defaults to 0")
       opts.on('-h', '--help', 'Prints this help') do
         puts opts
         exit
       end
-    end.parse!(into: params)
+    end.parse!(into: options)
+    params = DEFAULTS.merge(options)
 
     raise OptionParser::MissingArgument if params[:file].nil?
 
-    set_groups(params)
+    save_groups(params)
 
     params
   end
 
   # Sets the group constrains on a hash of its own
   # within params for easy access
-  def set_groups(hash)
-    group_keys = [ :min_short_term_percent,
-      :min_medium_term_percent,
-      :min_long_term_percent ]
-    min_groups = hash.select {|k, v| group_keys.include?(k) }
+  # @param [Hash] containing all params
+  # @param [Hash] containing :groups key with aggregated groups
+  def save_groups(hash)
+    group_keys = %i[min_short_term_percent
+                    min_medium_term_percent
+                    min_long_term_percent]
+    min_groups = hash.select { |k, _v| group_keys.include?(k) }
     hash[:groups] = min_groups
     hash
   end
 
+  # Reads data for a file
+  # @return [Array] src data given in input file
   def read_src_data
     file = read_input_file
     return unless file
+
     parse_input(file)
   end
 
+  # Reads input requested by user via $stdin
+  # @returns [String] string of data
+  # @raise [SystemCallError] if there is an error reading input file
   def read_input_file
     File.read(@params[:file])
   rescue SystemCallError => e
@@ -70,6 +107,10 @@ class InputParser
     nil
   end
 
+  # Parses JSON input string into Ruby objects
+  # @param [String] src string
+  # @return [Array] Ruby array with src projects
+  # @raise [JSON::ParserError] if src string is not valid JSON
   def parse_input(src)
     JSON.parse(src, symbolize_names: true)
   rescue JSON::ParserError => e
@@ -77,8 +118,10 @@ class InputParser
     nil
   end
 
+  # Writes data into a file set at input
+  # @param [Array] data source with purchase plan and CO2 report
   def write_output_file(data)
-    File.open(@params[:target], "w") do |f|
+    File.open(@params[:target], 'w') do |f|
       f.write(JSON.pretty_generate(data))
     end
   end
